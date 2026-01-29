36d25561ee3f59e66373f980aa5743b6b500b918
michaelperryjnr <michaelperryt97@gmail.com>
2026-01-29 20:03:28 +0000

    fixed view

--- GeeksForGeeks_Union_Of_Array_With_Duplicates.py
+++ GeeksForGeeks_Union_Of_Array_With_Duplicates.py
@@ -9,7 +9,8 @@ NOTES:
 i tried to use solve it using just the arrays but it was getting too complicated
 so I used sets to make it easier
 basically we convert both arrays to sets to remove duplicates
-then the set union method is used to find the union of both sets, this is inbuilt in python which makes it easy
+then the set union method is used to find the union of both sets, 
+this is inbuilt in python which makes it easy
 finally we convert the result back to a list and return it
 """
 class Solution:
