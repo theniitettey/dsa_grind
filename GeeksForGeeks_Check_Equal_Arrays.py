b71233aa9c6584bbc725eb3f9001d3eef909a2ea
michaelperryjnr <michaelperryt97@gmail.com>
2026-01-29 20:03:12 +0000

    fixed view

--- GeeksForGeeks_Check_Equal_Arrays.py
+++ GeeksForGeeks_Check_Equal_Arrays.py
@@ -6,16 +6,22 @@ problem_link: https://www.geeksforgeeks.org/problems/check-if-two-arrays-are-equ
 tries: 1
 
 NOTES:
-i thought of sorting both arrays and comparing them but that would take O(n log n) time, too much time in my opinion for this problem
-i tried to think of a way to do it without using extra space but i couldn't come up with anything, maybe in the future
-so i was like since the question says the arrays are equal if both contain the same set of elementts, arrangements(or permutations) of elements don't matter
+i thought of sorting both arrays and comparing them but that would take O(n log n) time, 
+too much time in my opinion for this problem
+i tried to think of a way to do it without using extra space but i couldn't come up with anything, 
+maybe in the future
+so i was like since the question says the arrays are equal if 
+both contain the same set of elementts, arrangements(or permutations) of elements don't matter
 i thought of using a dictionary to count the frequency of each element in the first array
 then i loop through the second array and decrease the frequency of each element found in the dictionary
-if an element is not found in the dictionary, we can return false right away, because that means the arrays are not equal, obviously
-then i realized i tried to do it by checking if a frequency becomes zero and trying to return false if an element is found again in the second array 
+if an element is not found in the dictionary, we can return false right away, 
+because that means the arrays are not equal, obviously
+then i realized i tried to do it by checking if a frequency becomes zero 
+and trying to return false if an element is found again in the second array 
 but a cleaner way is to just delete the element from the dictionary when its frequency becomes zero,
 then our first condition will take care of it
-finally, with my approach, if the dictionary is empty at the end, that means both arrays are equal, otherwise they are not
+finally, with my approach, if the dictionary is empty at the end, that means both arrays are equal, 
+otherwise they are not
 so we just return if the dictionary is empty or not
 sigh...
 """
